26baf0b535ad21d32c5b7d644da4736928f758d7
stay1st <joshua.crawford.mail@gmail.com>
2022-09-09 18:09:51 -0400

    When special character is noticed. now displays below text area

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React, { useState } from "react";
-import UserInput from "./UserInput"
+import RegExpCheck from "./RegExpCheck";
+
 
 
 class App extends React.Component {
-
-  handleChange(){
+  constructor(props) {
+    super(props);
+    this.state = {}
+    console.log('App props:', this.props);
   }
-  
+
+  createJSX() {
+    return this.state.regex.toString()
+  }
+
+  onChange(name, event, regex){
+    
+    this.setState({[name]: regex});
+    this.setState({input: event});
+    this.createJSX(name, event, regex);
+
+    this.createJSX()
+
+    console.log('App.onChange(name):', name)
+    console.log('App.onChange(event):', event)
+    console.log('App.onChange(regex):', regex)
+  }
+
 
   render() {
     return (
       <div className="App">
         <h2>Markdown Previewer</h2>
         <div>
-          <UserInput />
+          <RegExpCheck onChange={this.onChange.bind(this)} />
+          {console.log('App.onChange(state):', this.state)}
         </div>
         <div>
-
+        <p>{this.state.input}</p>
         </div>
-
       </div>
     )
   };
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,98 +1,86 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 
-export default function UserInput() {
+// export default function UserInput() {
 
-    const [htmlAttr, setHtmlAttr] = useState(<></>);
-    const [input, setInput] = useState('Hello');
-    // const expressions = {
-    //     h1: [/^#\s.*/, jsxObj.h1()],
-    //     h2: [/^#{2}\s.*/, jsxObj.h2()],
-    //     h3: [/^#{3}\s.*/, jsxObj.h3()],
-    //     h4: [/^#{4}\s.*/, jsxObj.h4()],
-    //     bold: [/[\*]{2}[a-zA-Z0-9\t\n ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+[\*]/g, jsxObj.bold()]
-    // };
+//     const [htmlAttr, setHtmlAttr] = useState(<></>);
+//     const [input, setInput] = useState('Hello');
+//     // const expressions = {
+//     //     h1: [/^#\s.*/, jsxObj.h1()],
+//     //     h2: [/^#{2}\s.*/, jsxObj.h2()],
+//     //     h3: [/^#{3}\s.*/, jsxObj.h3()],
+//     //     h4: [/^#{4}\s.*/, jsxObj.h4()],
+//     //     bold: [/[\*]{2}[a-zA-Z0-9\t\n ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+[\*]/g, jsxObj.bold()]
+//     // };
 
-    const handleChange = (event) => {
+//     const boldConvert = (input) => {
 
-        const boldConvert = (input) => {
+//         let astrics = '**'
 
-            let regInput = ''
+//         astrics = input.match(/[\*]{2}/g);
 
-            regInput = input.match(/[\*]{2}[a-zA-Z0-9\t\n ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+[\*]/g);
-            regInput = regInput.toString();
+//         // return <p>{beforeAstr}<strong>{middleExp}</strong>{afterAstr}</p>;
+//     };
 
-            let beforeAstr = input.split(regInput)[0];
-            let middleExp = regInput.replace(new RegExp(/\*\*/, 'g'), '');
-            let afterAstr = input.split(regInput)[2];
+//     const handleChange = (event) => {
 
-            // console.log('regInput-------------START:', regInput);
-            // console.log('beforeAstr BOLD:', beforeAstr);
-            // console.log("middleExp BOLD:", middleExp);
-            // console.log("aftrAst BOLD:", afterAstr);
-            // console.log("input in func:", input);
-            // console.log("input match:---------END", input.match(/[\*]{2}[a-zA-Z0-9\t\n ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+[\*]/g));
+//         function h1(input){
+//             let match = input.match(/^#\s.*/);
+//             input = input.replace('# ', ''); 
+//             return <h1>{input}{console.log("h1-Match:", match)}</h1> 
+//         };
+//         function h2(input){
+//             let match = input.match(/^#{2}\s.*/)
+//             input = input.replace('##', ''); 
+//             return <h2>{input}{console.log("h2-Match:", match)}</h2> 
+//         };
+//         function h3(input){ 
+//             let match = input.match(/^#{3}\s.*/)
+//             input = input.replace('###', ''); 
+//             return <h3>{input}{console.log("h3-Match:", match)}</h3>
+//         };
+//         function h4(input){
+//             let match = input.match(/^#{4}\s.*/)
+//             input = input.replace('####', ''); 
+//             return <h3>{input}{console.log("h4-Match:", match)}</h3>
+//         };
+//         function strong(input){
+//             let match = input.match(/[\*]{2}[a-zA-Z0-9\t\n ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+[\*]/g)
+//             return boldConvert(input)
+//         };
 
-            return <p>{beforeAstr}<strong>{middleExp}</strong>{afterAstr}</p>;
-        };
+//         setHtmlAttr(event.target.value);
+//         setInput(event.target.value);
 
-        function h1(input){
-            let match = input.match(/^#\s.*/);
-            input = input.replace('# ', ''); 
-            return <h1>{input}{console.log("h1-Match:", match)}</h1> 
-        };
-        function h2(input){
-            let match = input.match(/^#{2}\s.*/)
-            input = input.replace('##', ''); 
-            return <h2>{input}{console.log("h2-Match:", match)}</h2> 
-        };
-        function h3(input){ 
-            let match = input.match(/^#{3}\s.*/)
-            input = input.replace('###', ''); 
-            return <h3>{input}{console.log("h3-Match:", match)}</h3>
-        };
-        function h4(input){
-            let match = input.match(/^#{4}\s.*/)
-            input = input.replace('####', ''); 
-            return <h3>{input}{console.log("h4-Match:", match)}</h3>
-        };
-        function strong(input){
-            let match = input.match(/[\*]{2}[a-zA-Z0-9\t\n ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+[\*]/g)
-            return boldConvert(input)
-        };
+//     const arrRegEx = [/(?!.*)(?=\*{2})(.*)(?!\*{2})/g];
 
-        setHtmlAttr(event.target.value);
-        setInput(event.target.value);
+//         const callBacks = [h1(), h2(), h3(), h4(), strong()];
 
-        const arrRegEx = [/^#\s.*/, /^#{2}\s.*/, /^#{3}\s.*/, /^#{4}\s.*/, /[\*]{2}[a-zA-Z0-9\t\n ./<>?;:"'`!@#$%^&*()\[\]{}_+=|\\-]+[\*]/g];
+//         let text = ''
 
-        const callBacks = [h1(), h2(), h3(), h4(), strong()];
+//         for (let i = 0; i < arrRegEx.length; i++) {
 
-        let text = ''
+//             let text = input.match(arrRegEx[i])
 
-        for (let i = 0; i < arrRegEx.length; i++) {
-
-            let text = input.match(arrRegEx[i])
-
-            if (text !== null) {
-                text = callBacks[i](text);
-                setHtmlAttr(text);
-            }
-        }
-    }
-    return (
-        <div>
-            <textarea
-                id="editor"
-                onChange={handleChange}
-                value={input}
-                cols='40'
-                rows='20'
-            />
-            {htmlAttr}
-        </div>
-    )
-};
+//             if (text !== null) {
+//                 text = callBacks[i](text);
+//                 setHtmlAttr(text);
+//             }
+//         }
+//     }
+//     return (
+//         <div>
+//             <textarea
+//                 id="editor"
+//                 onChange={handleChange}
+//                 value={input}
+//                 cols='40'
+//                 rows='20'
+//             />
+//             {htmlAttr}
+//         </div>
+//     )
+// };
 
     // (input) {
         //         let regInput = '';
